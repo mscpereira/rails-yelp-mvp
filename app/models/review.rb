1c24c44b7210d2655b02826ea75ab90e818b44ea
mario pereira <marioscpereira@gmail.com>
2021-11-17 15:09:09 +0000

    simple restaurant with reviews app

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, inclusion: { in: 0..5, message: 'should be between 0 to 5' }
+  validates :rating, presence: { message: 'cannot be blank' }
+  validates :rating, numericality: { only_integer: true, message: 'should be a number' }
+  validates :content, presence: true
+end
